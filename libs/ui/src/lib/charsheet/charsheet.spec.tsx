d6c4790067a906cfa34d1b1d089f85aa649a8a03
oldgeese <hiro.onoda@gmail.com>
2022-05-21 16:31:33 +0900

    fix spec

--- libs/ui/src/lib/charsheet/charsheet.spec.tsx
+++ libs/ui/src/lib/charsheet/charsheet.spec.tsx
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react';
+import { newCharacter } from '@wi-charsheet/character';
 
 import Charsheet from './charsheet';
 
 describe('Charsheet', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Charsheet />);
+    const { baseElement } = render(<Charsheet character={newCharacter()}/>);
     expect(baseElement).toBeTruthy();
   });
 });
--- libs/ui/src/lib/general-information/general-information.spec.tsx
+++ libs/ui/src/lib/general-information/general-information.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
-
+import { newGeneralInformation } from '@wi-charsheet/general-information';
 import GeneralInformation from './general-information';
 
 describe('GeneralInformation', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<GeneralInformation />);
+    const { baseElement } = render(<GeneralInformation generalInformation={newGeneralInformation()}/>);
     expect(baseElement).toBeTruthy();
   });
 });
