acaacda5bfad7a9888ab4b1b189df01927a787cb
oldgeese <hiro.onoda@gmail.com>
2022-05-21 20:52:16 +0900

    add config for <ThemeProvider>

---
+++ libs/ui/.storybook/preview.js
@@ -0,0 +1,24 @@
+import {ThemeProvider} from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { theme } from '/apps/wi-charsheet/src/app/styles/theme'
+
+export const decorators = [
+    (Story) => {
+      return (
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Story />
+        </ThemeProvider>
+      )
+    },
+]
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
