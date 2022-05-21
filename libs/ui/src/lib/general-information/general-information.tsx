b828fb84fcdf09d757cb32343be674028c3a7325
oldgeese <hiro.onoda@gmail.com>
2022-05-21 16:36:26 +0900

    add label

--- libs/ui/src/lib/general-information/general-information.tsx
+++ libs/ui/src/lib/general-information/general-information.tsx
@@ -12,17 +12,17 @@ export function GeneralInformation(props: GeneralInformationProps) {
   const p = props.generalInformation
   return (
     <StyledGeneralInformation>
-      <div>{p.plName}</div>
-      <div>{p.pcName}</div>
-      <div>{p.race}</div>
-      <div>{p.age.toString()}</div>
-      <div>{p.gender}</div>
-      <div>{p.shapeAndOriginOfCapacity}</div>
-      <div>{p.reason}</div>
-      <div>{p.appearance}</div>
+      <div>PL:{p.plName}</div>
+      <div>名:{p.pcName}</div>
+      <div>種族:{p.race}</div>
+      <div>年齢:{p.age.toString()}</div>
+      <div>性別:{p.gender}</div>
+      <div>&lt;器&gt;の形状と由来:{p.shapeAndOriginOfCapacity}</div>
+      <div>フェノメナでいる理由:{p.reason}</div>
+      <div>外見の特徴など:{p.appearance}</div>
       <div>{p.keys.map((key) =>
         <div>
-          <div>{key.name}</div>
+          <div>キー:{key.name}</div>
           <div>{key.used.toString()}</div>
         </div>
       )}</div>
