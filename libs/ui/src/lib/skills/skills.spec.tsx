4a1ff07b6518b0bd78fd59f910e7f88126beb3c8
oldgeese <hiro.onoda@gmail.com>
2022-06-05 22:48:26 +0900

    implement skills spec

--- libs/ui/src/lib/skills/skills.spec.tsx
+++ libs/ui/src/lib/skills/skills.spec.tsx
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react';
+import { newSkills } from "@wi-charsheet/skills";
 
 import Skills from './skills';
 
 describe('Skills', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Skills />);
+    const { baseElement } = render(<Skills skills={newSkills()}/>);
     expect(baseElement).toBeTruthy();
   });
 });
