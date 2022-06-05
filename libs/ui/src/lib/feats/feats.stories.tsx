850423050bc68a60c8150274af3251bf442f1e8c
oldgeese <hiro.onoda@gmail.com>
2022-06-05 22:48:05 +0900

    implement feats stories

--- libs/ui/src/lib/feats/feats.stories.tsx
+++ libs/ui/src/lib/feats/feats.stories.tsx
@@ -1,4 +1,5 @@
 import { Story, Meta } from '@storybook/react';
+import { newFeats } from "@wi-charsheet/feats";
 import { Feats, FeatsProps } from './feats';
 
 export default {
@@ -9,4 +10,6 @@ export default {
 const Template: Story<FeatsProps> = (args) => <Feats {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  feats: newFeats()
+};
