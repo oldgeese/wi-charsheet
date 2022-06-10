5171280225fca93b714371ce4c49abafd37bfd04
oldgeese <hiro.onoda@gmail.com>
2022-06-10 22:23:06 +0900

    feat: add routes

--- apps/wi-charsheet/src/app/app.tsx
+++ apps/wi-charsheet/src/app/app.tsx
@@ -4,6 +4,11 @@ import { Character, newCharacter } from '@wi-charsheet/character';
 import { Dashboard } from '@wi-charsheet/ui';
 import { useEffect, useState } from 'react';
 import { theme } from './styles/theme';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom"
 
 const StyledApp = styled.div` `
 
@@ -18,7 +23,18 @@ export function App() {
   return (
     <ThemeProvider theme={theme}>
       <StyledApp>
-        <Dashboard characters={characters}/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Dashboard characters={characters}/>} >
+              <Route index element={<Dashboard characters={characters}/>} />
+              {/*
+              <Route path="newchar" element={<NewChar />} />
+              <Route path="viewchar/:id" element={<ViewChar />} />
+              <Route path="editchar/:id" element={<EditChar />} />
+              */}
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </StyledApp>
     </ThemeProvider>
   );
