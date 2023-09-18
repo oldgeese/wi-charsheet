a2ceb25a47f11b81c68748fc47b6432a437a3fc0
oldgeese <hiro.onoda@gmail.com>
2023-09-18 19:32:36 +0900

    feat: add charsheet and remove wi-charsheet app

--- apps/charsheet/src/app/app.tsx
+++ apps/charsheet/src/app/app.tsx
@@ -1,52 +1,42 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import styled from '@emotion/styled';
+import { ThemeProvider } from '@mui/material/styles';
+import { Character } from '@wi-charsheet/character';
+import { getAll } from '@wi-charsheet/service';
+import { Dashboard, EditChar, NewChar, ViewChar } from '@wi-charsheet/ui';
+import { useEffect, useState } from 'react';
+import {
+    BrowserRouter, Route, Routes
+} from "react-router-dom";
+import { theme } from './styles/theme';
 
-import NxWelcome from './nx-welcome';
-
-import { Route, Routes, Link } from 'react-router-dom';
+const StyledApp = styled.div` `
 
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="charsheet" />
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
-    </div>
+  const [characters, setCharacters] = useState<Character[]>([])
+
+  useEffect(() => {
+    const getAllCharacters = async () => {
+      const all = await getAll()
+      setCharacters(all)
+    }
+    getAllCharacters()
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StyledApp>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Dashboard characters={characters}/>} />
+            <Route index element={<Dashboard characters={characters}/>} />
+            <Route path="newchar" element={<NewChar />} />
+            <Route path="viewchar/:id" element={<ViewChar />} />
+            <Route path="editchar/:id" element={<EditChar />} />
+          </Routes>
+        </BrowserRouter>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
