278e80860710a3856c6a9a1c83c2ed5a777d4ebb
oldgeese <hiro.onoda@gmail.com>
2023-09-18 16:29:20 +0900

    feat: upgrade nx and etc.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
   stories: [],
-  addons: ['@storybook/addon-essentials'],
-  // uncomment the property below if you want to apply some webpack config globally
-  // webpackFinal: async (config, { configType }) => {
-  //   // Make whatever fine-grained changes you need that should apply to all storybook configs
-
-  //   // Return the altered config
-  //   return config;
-  // },
+  addons: [],
+  docs: {
+    autodocs: true
+  }
 };
